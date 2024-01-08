e37c4e27fd9246b155bf4d7e00c51e9c65f3950f
Ahmad Siddiqui <reachahmadsiddiqui@gmail.com>
2024-01-08 20:53:25 +0500

    changes

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.get("/", (req, res) => {
 });
 
 // Static Files
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 // Error Handler Middleware
